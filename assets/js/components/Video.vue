6e55e9c83c8d998db15237f6f5ab98e7c753d6ef
Dakeyras3 <dakeyras3@protonmail.com>
2019-03-06 17:38:38 +0100

    Player Component

--- assets/js/components/Video.vue
+++ assets/js/components/Video.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="videoPlayer">
-      <b-embed autoplay
+      <video autoplay
+             ref="player"
              width="100%"
              height="100%">
-        <source :src="video"/>
-      </b-embed>
+        <source :src="video" type="video/mp4"/>
+      </video>
     </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import { EventBus } from '../utils/event-bus.js'
 export default {
   data () {
       return {
@@ -18,7 +21,29 @@ export default {
     video: {
       type: String,
       required: true
+    },
+    timeCode: {
+      type: Number,
+      default: 0
     }
+  },
+  methods : {
+    ...mapActions({
+      setDisplayChoices: 'globiFmv/setDisplayChoices'
+    })
+  },
+  created () {
+    this.interval = setInterval(() => {
+      EventBus.$emit('video-timer', this.$refs.player.currentTime)
+    }, 500)
+  },
+  watch: {
+    video : function (value) {
+      this.$refs.player.src = value
+    }
+  },
+  destroyed: function () {
+    clearInterval(this.interval)
   }
 }
 </script>
