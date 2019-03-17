2c83eb0a9de5c92f9f038168b94d111019bbd1b3
Dakeyras3 <dakeyras3@protonmail.com>
2019-03-17 13:36:23 +0100

    Add config for the video (volume and quality)

--- assets/js/components/Video.vue
+++ assets/js/components/Video.vue
@@ -1,17 +1,33 @@
 <template>
     <div class="videoPlayer">
+      <b-button class="config" v-b-modal.config><icon name="cog" scale="2"/></b-button>
       <video autoplay
              ref="player"
              width="100%"
              height="100%">
         <source :src="video" type="video/mp4"/>
       </video>
+      <b-modal id="config"
+               title="Configuration"
+               ok-only ok-title="Ok"
+               ok-variant="outline-dark">
+        <p>Qualité</p>
+        <b-button variant="outline-dark" @click="setQuality('SD')">SD</b-button>
+        <b-button variant="outline-dark" @click="setQuality('HD')">HD</b-button>
+        <p>Volume</p>
+        <b-form-input type="range"
+                      step="0.01"
+                      min="0"
+                      max="1"
+                      v-model="volume"/>
+      </b-modal>
     </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 import { EventBus } from '../utils/event-bus.js'
+import 'vue-awesome/icons/cog'
 export default {
   data () {
       return {
@@ -25,8 +41,22 @@ export default {
   },
   methods : {
     ...mapActions({
-      setDisplayChoices: 'globiFmv/setDisplayChoices'
+      setQuality: 'globiFmv/setQuality'
     })
+  },
+  computed : {
+    volume : {
+      get () {
+        if(this.$refs.player) {
+          return this.$refs.player.volume
+        } else {
+          return '1'
+        }
+      },
+      set (value) {
+        this.$refs.player.volume = value
+      }
+    }
   },
   created () {
     this.interval = setInterval(() => {
@@ -48,5 +78,13 @@ export default {
 .videoPlayer{
   height: 100%;
   background-color: dimgrey;
+}
+
+.config {
+  position: absolute;
+  right: 0;
+  background: transparent;
+  border: none;
+  z-index: 10;
 }
 </style>
